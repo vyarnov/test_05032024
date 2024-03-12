cf945637a20b6cba94327dd2433c845257de9bd3
Vitaly Yarnov <vyarnov@gmail.com>
2024-03-12 16:25:35 +0300

    Add a new function ConvertArray

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] ConvertArray(string[] array)
+{
+    string[] newArray = new string[4];
+   
+    for (int i = 0, j = 0; i < array.Length; i++)
+    {
+        string res = array[i];
+        if (res.Length < 4)
+        {
+            newArray[j] = res;
+            j++;
+        }
+
+    }
+    return newArray;
+}
+
+
+string[] array = new string[] { "Hello", "2", "world", ":-)" };
+Console.WriteLine($"Массив исходный: [{string.Join(", ", array)}]");
+string[] newArray = ConvertArray(array);
+Console.WriteLine($"Массив измененный: [{string.Join(", ", newArray)}]");
