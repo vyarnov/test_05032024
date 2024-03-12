f8261e5cca3c64bf310e886605ec631ee914e033
Vitaly Yarnov <vyarnov@gmail.com>
2024-03-12 16:27:34 +0300

    Add a new function CountNumber

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿string[] ConvertArray(string[] array)
 {
-    string[] newArray = new string[4];
+    // string[] newArray = Array.Empty<string>();
+    string[] newArray = new string[CountNumber(array)];
    
     for (int i = 0, j = 0; i < array.Length; i++)
     {
@@ -8,6 +9,7 @@
         if (res.Length < 4)
         {
             newArray[j] = res;
+            // newArray = newArray.Append(res).ToArray();
             j++;
         }
 
@@ -15,6 +17,20 @@
     return newArray;
 }
 
+int CountNumber(string[] array)
+{
+    int sumNumber = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        string res = array[i];
+        if (res.Length < 4)
+        {
+            sumNumber++;
+        }
+
+    }
+    return sumNumber;
+}
 
 string[] array = new string[] { "Hello", "2", "world", ":-)" };
 Console.WriteLine($"Массив исходный: [{string.Join(", ", array)}]");
